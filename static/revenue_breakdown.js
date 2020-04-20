dda718dbc69b890785f8c5f9c6633ed68ee01f7f
Abbie Tran <abbie.y.tran99@gmail.com>
2020-04-19 22:20:55 -0400

    Revenue breakdown

---
+++ static/revenue_breakdown.js
@@ -0,0 +1,103 @@
+var margins = {tp: 20, btm: 180, lft: 80, rt: 30};
+var width = 600;
+var height = 600;
+
+function submission() {
+
+    var county = document.getElementById("county").value;
+    var chart = document.getElementById("vis");
+    getData()
+    .then(jsonTuples => { // renders the data table
+      createChart(jsonTuples, county, chart);
+    })
+    .catch(e => console.log(e));
+  }
+
+function createChart(jsonTuples, county, chart) {
+  data = filterData(county, jsonTuples);
+  var revenue = ["Total", "Federal", "State", "Local"];
+
+  var xBand = d3.scaleBand()
+    .domain(revenue)
+    .range([margins.lft, width - margins.rt])
+    .paddingInner(0.2);
+
+    var hScale = d3.scaleLinear()
+      .domain([1, data[0].value])
+      .range([height - margins.btm - margins.tp,0]);
+
+      d3.select(chart)
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", d => xBand(d.name) + "px")
+        .attr("y", d => margins.tp + hScale(d.value) + "px")
+        .attr("width", xBand.bandwidth())
+        .attr("height", d=> (height - margins.tp - margins.btm - hScale(d.value)))
+
+        .attr("fill", "red");
+
+        d3.select(chart).append("g")
+          .attr("transform",`translate(0,${height - margins.btm})`)
+          .call(d3.axisBottom(xBand))
+        
+
+
+
+        d3.select(chart).append("g")
+          .attr("transform", `translate(${margins.lft}, ${margins.tp})`)
+          .call(d3.axisLeft(hScale));
+          // Y-axis label
+        d3.select(chart).append("text")
+          .attr("x", 0 - height/2)
+          .attr("y", 15)
+          .attr("transform", "rotate(-90)")
+          .style("text-anchor", "middle")
+          .text("Revenue ($)");
+
+}
+
+function filterData(county, jsonTuples) {
+  var obj = [];
+  jsonTuples.forEach((j) => {
+    if (j.NAME === county) {
+      console.log("Hello there");
+      var total = {};
+      var federal = {};
+      var state = {};
+      var local = {};
+
+      total.value =j.TOTALREV;
+      total.name = "Total";
+
+      federal.value= j.TFEDREV;
+      federal.name = "Federal";
+
+      state.value = j.TSTREV;
+      state.name = "State";
+      local.value= j.TLOCREV;
+      local.name = "Local";
+
+      obj.push(total);
+      obj.push(federal);
+      obj.push(state);
+      obj.push(local);
+      return obj;
+    }
+  });
+  return obj;
+}
+function getData() {
+  return new Promise((resolve, reject) => {
+    var data_arr = [];
+    d3.json("http://localhost:8080/")
+
+    .then((json) => {
+      json.forEach( d => {
+        data_arr.push(d);
+           });
+        resolve(data_arr);
+    });
+  });
+}
