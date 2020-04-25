90a4e64946c686536c8df7f80dd09e6c9095aff4
Abbie Tran <abbie.y.tran99@gmail.com>
2020-04-25 16:53:29 -0400

    Added tooltips

--- static/js/revenue_breakdown.js
+++ static/js/revenue_breakdown.js
@@ -19,17 +19,21 @@ function createChart(jsonTuples, county, chart) {
   chart.innerHTML = "";
   data = filterData(county, jsonTuples);
   var revenue = ["Total", "Federal", "State", "Local"];
+  var colors = ["b82a04", "b82a04", "e1a61c", "040300"];
 
+  // xBand
   var xBand = d3.scaleBand()
     .domain(revenue)
     .range([margins.lft, width - margins.rt])
     .paddingInner(0.2);
 
+    // Height scale function
     var hScale = d3.scaleLinear()
       .domain([1, data[0].value])
       .range([height - margins.btm - margins.tp,0]);
 
       // Creates bar chart
+
       d3.select(chart)
         .selectAll("rect")
         .data(data)
@@ -47,12 +51,13 @@ function createChart(jsonTuples, county, chart) {
         return d.display;
       });
 
+      // Use http://bl.ocks.org/mstanaland/6100713 for stacked bar chart
+
+
         // X-axis
         d3.select(chart).append("g")
           .attr("transform",`translate(0,${height - margins.btm})`)
           .call(d3.axisBottom(xBand))
-
-
 
         // Y-axis
         d3.select(chart).append("g")
@@ -87,26 +92,34 @@ function filterData(county, jsonTuples) {
 
       federal.value= j.TFEDREV;
       federal.name = "Federal";
-
-      federal.display = "Compensatory(Title I): " + j.FEDRCOMP;
-      federal.display += "\n Children with disabilites: " + j.FEDRSPEC;
-      federal.display += "\n Child Nutrition Act: " + j.FEDRNUTR;
-      federal.display += "\n All other federal aid: " + j.FEDROTHR;
+      // Adding items for tooltip
+      federal.display = "Total federal funding: $" + j.TFEDREV;
+      federal.display += "\nCompensatory(Title I): $" + j.C14;
+      federal.display += "\n Children with disabilites: $" + j.C15;
+      federal.display += "\n Child Nutrition Act: $" + j.C25;
+      federal.display += "\n All other federal aid: $" + j.B13;
 
 
       state.value = j.TSTREV;
       state.name = "State";
-      state.display = "General formula assistance: " + j.STRFORM;
-      state.display = "\nSpecial education programs: " + j.STRSPEC;
-      state.display = "\nTransportation programs: " + j.STRTRANS;
-      state.display = "\nAll other state revenue: " + j.STROTHR;
+        // Adding items for tooltip
+      state.display = "Total state funding: $" + j.TSTREV;
+      state.display += "\nGeneral formula assistance: $" + j.C01;
+      state.display += "\nStaff improvement programs: $" + j.C04;
+      state.display += "\nSpecial education programs: $" + j.C05;
+      state.display += "\nTransportation programs: $" + j.C12;
+      state.display += "\nAll other state revenue: $" + j.C13;
 
       local.value= j.TLOCREV;
       local.name = "Local";
-      local.display = "Parent government contributions : " + j.T02;
-      local.display += "\nTransportation Fees: " + j.A08;
-      local.display += "\nSchool lunch revenues: " + j.A09;
-      local.display += "\nOther sales and service revenues: " + j.A20;
+        // Adding items for tooltip
+      local.display = "Total local funding: $" + j.TLOCREV;
+      local.display += "\nParent government contributions : $" + j.T02;
+      local.display += "\nTransportation Fees: $" + j.A08;
+      local.display += "\nSchool lunch revenues: $" + j.A09;
+      local.display += "\nOther sales and service revenues: $" + j.A20;
+      local.display += "\nInterest Earnings: $" + j.U22;
+      local.display += "\nOther local revenues: $" + j.U97;
 
       // Add objects to array
       obj.push(total);
