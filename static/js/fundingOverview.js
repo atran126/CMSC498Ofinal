89b9888e14baf2fdc4ac95d52ad9a1a8f45620a2
Amei Mbah <nottlynn@gmail.com>
2020-04-26 13:47:07 -0400

    fixed funding and revenue data

--- static/js/fundingOverview.js
+++ static/js/fundingOverview.js
@@ -1,139 +1,146 @@
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 150, left: 150},
-width = 600 - margin.left - margin.right,
-height = 400 - margin.top - margin.bottom;
+var margin = {
+        top: 30,
+        right: 30,
+        bottom: 150,
+        left: 150
+    },
+    width = 1500 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+var county = $("#dropdown").val();
 
-function submission() {
-    getData()
-    .then(data => { 
-      createChart(data);
-    })
-    .catch(e => console.log(e));
-}
+$(document).ready(function() {
+    d3.csv("http://localhost:8080/data/countyfunding.csv")
+        .then(data => {
 
-  
-function createChart(data) {
-    var county = document.getElementById("county").value;
-    var chart = document.getElementById("funding-overview");
-    console.log("county",county)
-   
-      // sort data
-    data.sort(function(b, a) {
-      return a.TOTALREV - b.TOTALREV;
-    });
+            $("#dropdown").change(function() {
+                county = this.value;
+            })
 
-    data.forEach((d) => console.log(d.NAME, d.TOTALREV));
-    
-    // append the svg object to the body of the page
-    var svgChart = d3.select(chart)
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+            console.log("hi");
+            createChart(data);
 
-    // X axis
-    var x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(data.map(function(d) { return d.NAME; }))
-    .padding(0.2);
-    svgChart.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+            function createChart(data) {
+                console.log("hi again");
+                // var county = document.getElementById("county").value;
+                var chart = document.getElementById("funding-overview");
+                // console.log("county", county)
 
-    // Y axis
-    var y = d3.scaleLinear()
-    .domain([0, data[0].TOTALREV])
-    .range([ height, 0]);
-    svgChart.append("g")
-    .call(d3.axisLeft(y));
+                // sort data
+                data.sort(function(b, a) {
+                    return a.TOTALREV - b.TOTALREV;
+                });
 
-    // Y Label
-    svgChart
-    .append('text')
-    .attr('class', 'label')
-    .attr('x', -(height /3) - margin.top)
-    .attr('y', -margin.left/2 )
-    .attr('transform', 'rotate(-90)')
-    .attr('text-anchor', 'middle')
-    .text('Total Funding ($)')
+                data.forEach((d) => console.log(d.NAME, d.TOTALREV));
 
-    // Bars
-    svgChart.selectAll("bars")
-    .data(data)
-    .enter()
-    .append("rect")
-      .attr("x", function(d) { return x(d.NAME); })
-      .attr("y", function(d) { return y(d.TOTALREV); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.TOTALREV); })
-      .attr("fill", function(d) {if (d.NAME == county) return "red"; else return "#69b3a2"})
-      .on('mouseenter', function(actual, i) {
-        d3.selectAll('.value')
-        .attr('opacity', 0)
+                // append the svg object to the body of the page
+                var svgChart = d3.select(chart)
+                    .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform",
+                        "translate(" + margin.left + "," + margin.top + ")");
 
-        const countyY = y(actual.TOTALREV)
-        console.log("selected", actual, countyY);
+                // X axis
+                var x = d3.scaleBand()
+                    .range([0, width])
+                    .domain(data.map(function(d) {
+                        return d.NAME;
+                    }))
+                    .padding(0.2);
+                svgChart.append("g")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x))
+                    .selectAll("text")
+                    .attr("transform", "translate(-10,0)rotate(-45)")
+                    .style("text-anchor", "end");
 
-        svgChart.append('line')
-          .attr('class', 'limit')
-          .attr('x1', 0)
-          .attr('y1', countyY)
-          .attr('x2', width)
-          .attr('y2', countyY);
+                // Y axis
+                var y = d3.scaleLinear()
+                    .domain([0, data[0].TOTALREV])
+                    .range([height, 0]);
+                svgChart.append("g")
+                    .call(d3.axisLeft(y));
+
+                // Y Label
+                svgChart
+                    .append('text')
+                    .attr('class', 'label')
+                    .attr('x', -(height / 3) - margin.top)
+                    .attr('y', -margin.left / 2)
+                    .attr('transform', 'rotate(-90)')
+                    .attr('text-anchor', 'middle')
+                    .text('Total Funding ($)')
+
+                // Bars
+                svgChart.selectAll("bars")
+                    .data(data)
+                    .enter()
+                    .append("rect")
+                    .attr("x", function(d) {
+                        return x(d.NAME);
+                    })
+                    .attr("y", function(d) {
+                        return y(d.TOTALREV);
+                    })
+                    .attr("width", x.bandwidth())
+                    .attr("height", function(d) {
+                        return height - y(d.TOTALREV);
+                    })
+                    .attr("fill", function(d) {
+                        if (d.NAME == county) return "red";
+                        else return "#69b3a2"
+                    })
+                    .on('mouseenter', function(actual, i) {
+                        d3.selectAll('.value')
+                            .attr('opacity', 0)
+
+                        const countyY = y(actual.TOTALREV)
+                        console.log("selected", actual, countyY);
+
+                        svgChart.append('line')
+                            .attr('class', 'limit')
+                            .attr('x1', 0)
+                            .attr('y1', countyY)
+                            .attr('x2', width)
+                            .attr('y2', countyY);
 
 
-          // svgChart.append('text')
-          // .data(data)
-          // .attr('class', "divergence")
-          // .attr('x', d => x(d.NAME) + x.bandwidth() / 2)
-          // .attr('y', d => y(d.TOTALREV) + 30)
-          // .attr('fill', 'white')
-          // .attr('text-anchor', 'middle')
-          // .text((d, idx) => {
-          //   const divergence = (d.TOTALREV - actual.TOTALREV).toFixed(1)
-    
-          //   let text = ''
-          //   if (divergence > 0) text += '+'
-          //   text += `${divergence}%`
-    
-          //   return idx !== i ? text : '';
-          // });
-      })
-      // .on('mouseleave', function() {
-      //   d3.selectAll('.value')
-      //     .attr('opacity', 1)
+                        // svgChart.append('text')
+                        // .data(data)
+                        // .attr('class', "divergence")
+                        // .attr('x', d => x(d.NAME) + x.bandwidth() / 2)
+                        // .attr('y', d => y(d.TOTALREV) + 30)
+                        // .attr('fill', 'white')
+                        // .attr('text-anchor', 'middle')
+                        // .text((d, idx) => {
+                        //   const divergence = (d.TOTALREV - actual.TOTALREV).toFixed(1)
 
-      //   d3.select(this)
-      //     .transition()
-      //     .duration(300)
-      //     .attr('opacity', 1)
-      //     .attr('x', (a) => x(a.language))
-      //     .attr('width', x.bandwidth())
-    
-      //     svgChart.selectAll("line-limit").remove()
-      //     svgChart.selectAll("divergence").remove()
-      // })
+                        //   let text = ''
+                        //   if (divergence > 0) text += '+'
+                        //   text += `${divergence}%`
 
-      
-};
+                        //   return idx !== i ? text : '';
+                        // });
+                    })
+                // .on('mouseleave', function() {
+                //   d3.selectAll('.value')
+                //     .attr('opacity', 1)
 
-function getData() {
-  return new Promise((resolve, reject) => {
-    var data_arr = [];
-    d3.json("http://localhost:8080/")
+                //   d3.select(this)
+                //     .transition()
+                //     .duration(300)
+                //     .attr('opacity', 1)
+                //     .attr('x', (a) => x(a.language))
+                //     .attr('width', x.bandwidth())
 
-    .then((json) => {
-      json.forEach( d => {
-        data_arr.push(d);
-            });
-        resolve(data_arr);
-    });
-  });
-}
-  
+                //     svgChart.selectAll("line-limit").remove()
+                //     svgChart.selectAll("divergence").remove()
+                // })
 
+
+            };
+        })
+        .catch(e => console.log(e));
+})
